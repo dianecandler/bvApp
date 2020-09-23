a0d26e900432ef56d4f909e63feb401acb8ba6f3
Diane Candler <dianedcandler@gmail.com>
2020-09-23 11:43:12 -0500

    carousel works, image size needed

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 // Components
 import Navbar from './components/Navbar';
+import Hero from './components/Wrapper/Hero';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
       <Footer />
     </div>
   );
