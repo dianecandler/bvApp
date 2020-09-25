90f3c704cde2fb970446fcc7d23ea8773ba6f700
Diane Candler <dianedcandler@gmail.com>
2020-09-25 11:43:29 -0500

    connected pages with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 // Components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import PageHome from './components/PageHome';
+import PageAbout from './components/PageAbout';
+import PageCalendar from './components/PageCalendar';
+import PagePhotoshoot from './components/PagePhotoshoot';
+import PageVenue from './components/PageVenue';
+import PageContact from './components/PageContact';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <PageHome />
+      <Router>
+        <Switch>
+          <Route exact path="/" ><PageHome /></Route>
+          <Route exact path="/venue-rental" ><PageVenue /></Route>
+          <Route exact path="/calendar" ><PageCalendar /></Route>
+          <Route exact path="/photoshoot" ><PagePhotoshoot /></Route>
+          <Route exact path="/about-us" ><PageAbout /></Route>
+          <Route exact path="/contact-us" ><PageContact /></Route>
+        </Switch>
+      </Router>
       <Footer />
     </div>
   );
---
+++ client/src/components/PageAbout/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Bartlett from '../Bartlett';
+import Gallery from '../Gallery';
+import Map from '../Map';
+import Contact from '../Contact';
+
+export default function index() {
+    return (
+        <div>
+            <Bartlett />
+            <Gallery />
+            <Map />
+            <Contact />
+        </div>
+    )
+}
