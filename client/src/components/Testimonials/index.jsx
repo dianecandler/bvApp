a08ec01ff00b79868c29040e9aa9ea4009d95a76
Diane Candler <dianedcandler@gmail.com>
2020-09-24 20:56:58 -0500

    Finished Testimonials - check carousel timing

--- client/src/components/Testimonials/index.jsx
+++ client/src/components/Testimonials/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import Client1 from './hero1.jpg';
+import Client1 from './moms.jpg';
+import Client2 from './Client2.jpg';
+import Client3 from './Client3.jpg';
 import {Carousel, Container, Row, Col, Card, } from 'react-bootstrap';
 
 export default function index() {
@@ -11,11 +13,11 @@ export default function index() {
                         <Col><img className="w-100" src={Client1} /></Col>
                         <Col>
                             <Card style={{border: "none"}}>
-                                <Card.Header>Quote</Card.Header>
+                                <Card.Header>Wedding Ceremony and Reception</Card.Header>
                                 <Card.Body>
                                     <blockquote className="blockquote mb-0">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                                        <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                                        <p>Mockingbird Market was the perfect place for my son's wedding ceremony.  We enjoyed bringing personal items and decorating the windows and display cabinet.  Pesky Rabbit was awesome for the reception and dance.  We set up Corn Hole in the Garden.  Such an easy event.  We even set up our BBQ grill in the alley.</p>
+                                        <footer className="blockquote-footer">Mary Gallander and Family <cite title="Source Title"></cite></footer>
                                     </blockquote>
                                 </Card.Body>
                             </Card>
@@ -26,14 +28,14 @@ export default function index() {
             <Carousel.Item>
                 <Container className="border border-dark p-3">
                     <Row>
-                        <Col><img className="w-100" src={Client1} /></Col>
+                        <Col><img className="w-100" src={Client2} /></Col>
                         <Col>
                         <Card style={{border: "none"}}>
-                                <Card.Header>Quote</Card.Header>
+                                <Card.Header>Independence Day Celebration</Card.Header>
                                 <Card.Body>
                                     <blockquote className="blockquote mb-0">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                                        <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                                        <p>We decided to bring family together to celebrate Independence Day this year.  We rented the Pesky Rabbit and brought in our own BBQ with all the fixings.  We used their system to play music and at the end of the evening we all went outside to light up sparklers.  Such a relaxing place.  Love the brick streets and old buildings.  Very cool.  We will return!</p>
+                                        <footer className="blockquote-footer">Christine Dry <cite title="Source Title">, Dry Family Party Planner</cite></footer>
                                     </blockquote>
                                 </Card.Body>
                             </Card>
@@ -44,14 +46,14 @@ export default function index() {
             <Carousel.Item>
                 <Container className="border border-dark p-3">
                     <Row>
-                        <Col><img className="w-100" src={Client1} /></Col>
+                        <Col><img className="w-100" src={Client3} /></Col>
                         <Col>
                         <Card style={{border: "none"}}>
-                                <Card.Header>Quote</Card.Header>
+                                <Card.Header>Baby Shower</Card.Header>
                                 <Card.Body>
                                     <blockquote className="blockquote mb-0">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                                        <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                                        <p>Our first grandchild is expected anytime.  We decided to give our daughter a very special baby shower and rented Mockingbird Market.  What a great place with wonderful light.  Our snapshots turned out amazing thanks to natural lighting.  We looked like professional photographers!  Lovely time and great venue hosts. Plenty of parking. Easy planning. Highly recommend this place!</p>
+                                        <footer className="blockquote-footer">Cathy Umiker<cite title="Source Title">, Jessica's proud Grandmother</cite></footer>
                                     </blockquote>
                                 </Card.Body>
                             </Card>
