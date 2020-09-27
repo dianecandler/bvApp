e806902f9ae2c8f53d1d80ed68873fdf9b46d6d1
Diane Candler <dianedcandler@gmail.com>
2020-09-26 21:10:44 -0500

    Updated Gallery on homePage

--- client/src/components/Gallery/photos.jsx
+++ client/src/components/Gallery/photos.jsx
@@ -1,57 +1,66 @@
 export const photos = [
     {
-      src: './images/Gallery/1prBar.jpg',
+      src: './images/Gallery/Gallery1.jpg',
       width: 4,
       height: 3
     },
     {
-      src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+      src: './images/Gallery/Gallery2.jpg',
+      // 800x799
       width: 1,
       height: 1
     },
     {
-      src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+      src: './images/Gallery/Gallery3.jpg',
+      // 600x799
       width: 3,
       height: 4
     },
     {
-      src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+      src: './images/Gallery/Gallery4.jpg',
+      // 600x799
       width: 3,
       height: 4
     },
     {
-      src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+      src: './images/Gallery/Gallery5.jpg',
       // <img src={5x600x799} />
       width: 3,
       height: 4
     },
     {
-      src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+      src: './images/Gallery/Gallery6.jpg',
+      // 800x599
       width: 4,
       height: 3
     },
     {
-      src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+      src: './images/Gallery/Gallery7.jpg',
+      //600x799
       width: 3,
       height: 4
     },
     {
-      src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+      src: './images/Gallery/Gallery8.jpg',
+      // 800x599
       width: 4,
       height: 3
     },
     {
-      src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+      src: './images/Gallery/Gallery9.jpg',
+        // 800x599
       width: 4,
       height: 3
     },
     {
-      src: "https://source.unsplash.com/XiDA78wAZVw/600x799",
+      src: './images/Gallery/Gallery10.jpg',
+      // 600x799
       width: 3,
       height: 4
     },
     {
-      src: "https://source.unsplash.com/x8xJpClTvR0/800x599",
+      src: './images/Gallery/Gallery11.jpg',
+      // 800x599
       width: 4,
       height: 3
     },
