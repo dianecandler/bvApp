a178a453ac37ffd1b97150acf0c2af2b3369e069
Diane Candler <dianedcandler@gmail.com>
2020-10-03 10:27:37 -0500

    Calendar Logic - get date input and pass the value to the global state function

--- client/src/components/PageCalendar/index.jsx
+++ client/src/components/PageCalendar/index.jsx
@@ -13,6 +13,47 @@ export default class index extends Component {
         showRegister: false,
         showConfirmation: false
     }
+
+    // handle input change function
+    handleInputChange = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+
+        const value = document.getElementById('search').value;
+        console.log(value);
+
+        this.searchDate(this.state.search);
+
+    };
+
+    searchDate = newDate => {
+        console.log(newDate);
+        let a = new Date(newDate)
+        let d = new Date();
+        this.setState({date:newDate});
+        console.log(this.state.date);
+        if(a < d ) {
+            this.setState({showProducts:false});
+            alert('Please select upcoming date to book us!');
+        } else if (newDate === undefined) {
+            this.setState({showProducts:false});
+            alert('Please select a date to book us!');
+        } 
+        else {
+            this.setState({showProducts: true, showCalendar: false});
+            // API.saveDate({_id:a, date:a}).then(saveDate => this.setState({date: saveDate})).catch(err => console.error(err));
+        }
+    };
+
+
+
     render() {
         return (
             <div>
